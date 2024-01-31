0258ad5837ff692ee49cd4d2d6c95b2b9151db4a
admin <admin@DESKTOP-U4VGM4A>
2024-02-01 01:12:23 +0300

    Добавьте файлы проекта.

---
+++ ToursApp/AddEditPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToursApp
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+   public partial class AddEditPage : Page
+    {
+        private Hotel _currentHotel = new Hotel();
+        public AddEditPage(Hotel selectedHotel)
+        {
+            InitializeComponent();
+
+            if (selectedHotel != null)
+                 _currentHotel = selectedHotel;
+
+                DataContext = _currentHotel;
+                ComboCountries.ItemsSource = ToursBaseEntities.GetContext().Country.ToList();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrEmpty(_currentHotel.Name))
+                errors.AppendLine("Input name of hotel");
+            if (_currentHotel.CountOfStars < 1 || _currentHotel.CountOfStars > 5)
+                errors.AppendLine("Quantity of stars: 1-5");
+            if (_currentHotel.Country == null)
+                errors.AppendLine("Choose a county");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (_currentHotel.Id == 0)
+            
+                ToursBaseEntities.GetContext().Hotel.Add(_currentHotel);
+            try
+            {
+                ToursBaseEntities.GetContext().SaveChanges();
+                MessageBox.Show("data save");
+                Manager.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
